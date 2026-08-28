d03bd563d69035c0c501815e2b1eaa77bd13fb4d
Farah Hamdan <fh115881@gmail.com>
2026-08-28 15:59:46 +0300

    update vite

--- src/component/DrawerApp.jsx
+++ src/component/DrawerApp.jsx
@@ -78,7 +78,7 @@ const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' 
 );
 const LogoImg = () => (
   <img
-    src="../src/imgs/one.jpg"
+    src="../public/imgs/farah.jpg"
     alt="Logo"
     style={{ width: '7%', height: '7%', borderRadius: '50%' }}
   />
