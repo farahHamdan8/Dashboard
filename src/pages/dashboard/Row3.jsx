782409ffc85be8a34bffc0836088dfadb581f2b2
Farah Hamdan <fh115881@gmail.com>
2026-08-29 11:54:27 +0300

    no

--- src/pages/dashboard/Row3.jsx
+++ src/pages/dashboard/Row3.jsx
@@ -7,13 +7,13 @@ import InvoicesChart from '../invoicesChart/InvoicesChart';
 const Row3 = () => {
   return (
     <Stack direction={'row'} sx={{ gap: 1, flexWrap: 'wrap' }}>
-      <Paper sx={{ width: '33%', flexGrow: 1 }}>
+      <Paper sx={{ width: '30%', flexGrow: 1 }}>
         <Pie isDashboard={true} />
       </Paper>
-      <Paper sx={{ width: { xs: '100%', sm: '33%' }, height: '55vh', flexGrow: 1 }}>
+      <Paper sx={{ width: { xs: '100%', sm: '30%' }, height: '55vh', flexGrow: 1 }}>
         <Bar isDashboard={true} />
       </Paper>
-      <Paper sx={{ width: '33%', flexGrow: 1 }}>
+      <Paper sx={{ width: '30%', flexGrow: 1 }}>
         <InvoicesChart isDashboard={true} />
       </Paper>
     </Stack>
